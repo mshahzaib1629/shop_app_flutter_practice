9ebd253bdbdaca68bae43e50e3ebc11ad1a8cdb9
mshahzaib1629 <mshahzaib1629@gmail.com>
2020-01-24 00:31:41 +0500

    Dismissible Cart Items
    Added Swip effect in Cart Screen and added detail in Product Detail page

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -1,31 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/cart.dart';
 
 class CartItem extends StatelessWidget {
   final String id;
+  final String productId;
   final double price;
   final int quantity;
   final String title;
 
-  CartItem(this.id, this.price, this.quantity, this.title);
+  CartItem(this.id, this.productId, this.price, this.quantity, this.title);
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-        margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-        child: Padding(
-          padding: EdgeInsets.all(8),
-          child: ListTile(
-            leading: CircleAvatar(
-              child: FittedBox(
-                  child: Padding(
-                padding: EdgeInsets.all(5),
-                child: Text('\$$price'),
-              )),
+    return Dismissible(
+      key: ValueKey(id),
+      direction: DismissDirection.endToStart,
+      background: Container(
+        child: Icon(
+          Icons.delete,
+          size: 40,
+          color: Colors.black,
+        ),alignment: Alignment.centerRight,
+        margin: EdgeInsets.only(right: 20),
+      ),
+      onDismissed: (direction) {
+        Provider.of<CartProvider>(context, listen: false).removeItem(productId);
+      },
+      child: Card(
+          margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+          child: Padding(
+            padding: EdgeInsets.all(8),
+            child: ListTile(
+              leading: CircleAvatar(
+                child: FittedBox(
+                    child: Padding(
+                  padding: EdgeInsets.all(5),
+                  child: Text('\$$price'),
+                )),
+              ),
+              title: Text(title),
+              subtitle: Text('Total: \$${price * quantity}'),
+              trailing: Text('$quantity x'),
             ),
-            title: Text(title),
-            subtitle: Text('Total: \$${price * quantity}'),
-            trailing: Text('$quantity x'),
-          ),
-        ));
+          )),
+    );
   }
 }
